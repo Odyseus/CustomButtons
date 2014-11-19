c44790deefa97be3faeb37318498898ca9abb098
Odyseus <venusyadonis@gmail.com>
2014-11-18 21:08:55 -0300

    ....................

---
+++ 00-0Notes to myself.js
@@ -0,0 +1,8 @@
+/**
+ *  #### Notes to myself ####
+ *  1- All Ajax requests throw errors when running from local fyle system. Ignore them.
+ *  Examples: "mismatched tag. Expected: </meta>." or "not well-formed".
+ *  2- Button URIs are added manually.
+ *  3- URLs to GitHub sources added manually.
+ *  
+ */
