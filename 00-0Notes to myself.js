fd12c321b54440137d0197a3510836daef5ffd45
Odyseus <venusyadonis@gmail.com>
2014-12-18 08:14:12 -0300

    ..............................

--- 00-0Notes to myself.js
+++ 00-0Notes to myself.js
@@ -1,8 +1,12 @@
 /**
  *  #### Notes to myself ####
  *  1- All Ajax requests throw errors when running from local fyle system. Ignore them.
- *  Examples: "mismatched tag. Expected: </meta>." or "not well-formed".
+ *     Examples: "mismatched tag. Expected: </meta>." or "not well-formed".
  *  2- Button URIs are added manually.
- *  3- URLs to GitHub sources added manually.
- *  4- Code preview is automatically generated from the button URI.
+ *  3- Name of HTML install pages and Master folders MUST BE EXACTLY THE SAME.
+ *  4- URLs to GitHub sources inserted on page load. EMPHASIS on point 3.
+ *  5- Code preview is automatically generated from the button URI.
+ *  6- #headmenu and #footer are inserted via jQuery from 00-CommonHTML.html file.
+ *  7- ALWAYS define variable "syntax" at the end of each install page even if button dosn't have a Help tab.
+ *  8- Remember to set the "active" tab for "Button code preview" section.
  */
