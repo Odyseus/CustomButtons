5a97b660d0ed251cd53e177316a20a596e0cac5f
Odyseus <venusyadonis@gmail.com>
2014-12-19 02:36:02 -0300

    ....................

--- 00-0Notes to myself.js
+++ 00-0Notes to myself.js
@@ -7,6 +7,10 @@
  *  4- URLs to GitHub sources inserted on page load. EMPHASIS on point 3.
  *  5- Code preview is automatically generated from the button URI.
  *  6- #headmenu and #footer are inserted via jQuery from 00-CommonHTML.html file.
- *  7- ALWAYS define variable "syntax" at the end of each install page even if button dosn't have a Help tab.
- *  8- Remember to set the "active" tab for "Button code preview" section.
+ *  7- Remember to set the "active" tab for "Button code preview" section.
+ *  8- ALWAYS declare variable "syntax" at the end of each install page even if button dosn't have a Help tab.
+ *  9- ALWAYS declare "isIndex" var.
+ *     0 = For install pages
+ *     1 = For index
+ *     2 = For any other pages.
  */
